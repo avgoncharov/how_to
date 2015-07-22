2b71b12467af83ea48f22446c4f0857c92e39dd3
avgoncharov <goncharov.andrey.v@gmail.com>
2015-07-22 22:47:51 +0300

    Change comment.

--- how_to/update_site/update_site.py
+++ how_to/update_site/update_site.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-# This script creates backup and updates site(or service) by simply copying of files and directories.
+# This script creates backup and updates site(or service) by simple copying of files and directories.
 import os
 import sys
 import time
